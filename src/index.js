cb90c3e029d4ad4623645078d6dc943721e32864
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-10-21 23:40:57 +0200

    Promise errors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,75 @@
 const puppeteer = require('puppeteer');
 
 (async function main(){
-    try{
-        const browser = await puppeteer.launch({headless: false});
-        const page = await browser.newPage();
-        page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36');
-        
-        await page.setCookie({
-          "value": "%5B%7B%22id%22%3A121900%2C%22label%22%3A%22Le%20Lignon%22%7D%5D",
-          "expires": 1510321201,
-          "domain": "www.meteosuisse.admin.ch",
-          "name": "met_last-locations"
-        })
-        
-        await page.goto('https://www.meteosuisse.admin.ch/home/meteo/previsions.html');
-        await page.waitForSelector('.section');
-        
-        const sections = await page.$$('.section');
-        console.log(sections.length);
-        
-        for(const section of sections){       
-            const button = await section.$('a.weather-widget__warning__tooltip');
+    
+    const location = parseInt(process.argv[2]); 
+    
+    if(location){
 
-            if(button != null){
-                button.click();
-            }      
+        try{
+
+            console.log(location);
+            const browser = await puppeteer.launch({headless: false});
+            const page = await browser.newPage();
+
+            // Act like nomal browser not as bot 
+            page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36');
+
+            // Navigate on current weather widget and wait for link
+            await page.goto('https://www.meteosuisse.admin.ch/home/meteo/previsions.html');
+            await page.waitForSelector('a.weather-widget__warning__tooltip');
+
+            
+            // Get form to set location
+            const button_change_location = await page.$('a.weather-widget__warning__tooltip');
+            button_change_location.click();
+            await page.waitForSelector('a.weather-widget__warning__tooltip');
+            
+
+            // Write location with postal code and show dropdown choices    
+            await page.evaluate((location) => {
+                document.querySelector('input.locationChangeInput').value = location;
+            }, location);
+            await (await page.$( 'input.locationChangeInput' )).press('ArrowLeft');
+
+            // Check and get location from user
+            const options = await page.$$('.autocomplete-list > li');
+            const location_infos = await page.evaluate(el => el.innerText, options[0]);
+
+            if(options.length == 0 || location_infos == "Localité introuvable"){
+                console.log('Postal code not found');
+                process.exit(1);
+            }else{
+
+                // Get location infos
+                const postal_code = location_infos.substring(0, 4);
+                const state = location_infos.substring(5);
+
+                // Set location with postal code
+                const button_set_location = await page.$('input.locationChangeSubmit');
+                button_set_location.click();
+                await page.waitForSelector('input.locationChangeSubmit');
+
+                // Navigate on current weather widget and wait for link
+                await page.goto('https://www.meteosuisse.admin.ch/home/meteo/previsions.html');
+                await page.waitForSelector('.weather-widget__title');
+
+                // Get current weather infos                
+                const city = await page.evaluate(el => el.innerText, await page.$('.weather-widget__title'));
+                const c_temp = await page.evaluate(el => el.innerText, await page.$('.weather-widget__actual-weather__temp'));
+                const desc = await page.evaluate(el => el.innerText, await page.$('.weather-widget__actual-weather__desc'));
+                console.log(city + ' ' + desc + ' ' + c_temp);
+
+            }
+            
+
+        }catch(error){
+            console.log('Error: ', error)
+
         }
-        
-        
-        /*const ass = await page.$$('#weather-widget > a');
-        console.log(ass.length);
-        
-        for(const section of ass){
-            const button = await section.$('a.weather-widget__warning__tooltip');
-            button.click();
-        }*/
-        
-        
-    }catch(error){
-        console.log('error: ', error)
-        
-}
+    }else{
+        console.log('You must add a valid swiss postal code');
+        process.exit(1);
+    }
+    await browser.close();
 })();
