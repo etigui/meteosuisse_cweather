cfe390fcf0d19e752452ef3114f3ffa557150e19
raccoonmaster <32940684+raccoonmaster@users.noreply.github.com>
2018-10-23 12:49:49 +0200

    Download datas weather ok

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const fs = require('fs');
     if(location){
 
         try{
-
-            console.log(location);
             const browser = await puppeteer.launch({headless: true});
             const page = await browser.newPage();
 
@@ -21,7 +19,6 @@ const fs = require('fs');
             await page.goto(swiss_meteo_url);
             await page.waitForSelector('a.weather-widget__warning__tooltip');
 
-            
             // Get form to set location
             const button_change_location = await page.$('a.weather-widget__warning__tooltip');
             button_change_location.click();    
@@ -31,11 +28,16 @@ const fs = require('fs');
                 document.querySelector('input.locationChangeInput').value = location;
             }, location);
             await (await page.$( 'input.locationChangeInput' )).press('ArrowLeft');
-
-            // Check user location
-            const options = await page.$$('.autocomplete-list > li');
-            if(options){
+            await page.waitForSelector('.autocomplete-list');
+            
+            // Check if Autocomplete list exist (chown)
+            try {
+                await page.waitForSelector('.autocomplete-list')
                 
+                            // Check user location
+            const options = await page.$$('.autocomplete-list > li');
+            if(options !== undefined){
+                            
                 // Get user location
                 const location_infos = await page.evaluate(el => el.innerText, options[0]);
                 if(options.length == 0 || location_infos == "Localité introuvable"){
@@ -45,8 +47,8 @@ const fs = require('fs');
 
                     // Set location with postal code
                     await page.evaluate(() => {
-                        const element = document.querySelector('input.locationChangeSubmit');
-                        element.click();
+                        const el = document.querySelector('input.locationChangeSubmit');
+                        el.click();
                         return true;
                     });
 
@@ -54,68 +56,87 @@ const fs = require('fs');
                     await page.goto(swiss_meteo_url);
                     await page.waitForSelector('.weather-widget__title');
 
-                    // Get current weather infos                
+                    // Get current weather infos
                     const c_city = await page.evaluate(el => el.innerText, await page.$('.weather-widget__title'));
                     const c_temp = await page.evaluate(el => el.innerText, await page.$('.weather-widget__actual-weather__temp'));
                     const c_desc = await page.evaluate(el => el.innerText, await page.$('.weather-widget__actual-weather__desc'));
-                    const c_date = await page.evaluate(el => el.innerText, await page.$('.weather-widget__date'));
+                    let c_date = await page.evaluate(el => el.innerText, await page.$('.weather-widget__date'));
+                    const c_icon = await page.evaluate(el => el.src, await page.$('.weather-widget__actual-weather__img'));
                     
-                    
-                    /*const img = await page.$$('.weather-widget__forecast__list-item > img');
-                    console.log(img.length);
-                    for(let i = 0; i < img.length; i++){
-                        const x = await page.evaluate(el => el.src, img[i]);
-                        console.log(x);
-                    }*/
-                    
-                    const data = await page.evaluate(() => {
-                        const tds = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > strong'))
-                        return tds.map(td => td.innerText)
+                    // Format time and date
+                    c_date = c_date.replace(',','.').replace(' ','');
+                    const c_time = c_date.split('.')[1];
+                    c_date = c_date.split('.')[0] + ".";
+                                      
+                    // Forecast icon
+                    const f_icons = await page.evaluate(() => {
+                        const els = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > img'));
+                        return els.map(el => el.src);
                     });
                     
-                    
-                    const datas = await page.evaluate(() => {
-                        const tds = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > img'))
-                        return tds.map(td => td.src)
+                    // Forecast icon alt text
+                    const f_icons_desc = await page.evaluate(() => {
+                        const els = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > img'));
+                        return els.map(el => el.title);
                     });
                     
-                    
-                    const datad = await page.evaluate(() => {
-                        const tds = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > img'))
-                        return tds.map(td => td.title)
+                    // Forecast temp
+                    const f_temp = await page.evaluate(() => {
+                        const els = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > span'));
+                        return els.map(el => el.innerText);
                     });
                     
-                    
-                    const dataf = await page.evaluate(() => {
-                        const tds = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > span'))
-                        return tds.map(td => td.innerText)
+                    // Forecast day
+                    const f_date = await page.evaluate(() => {
+                        const els = Array.from(document.querySelectorAll('.weather-widget__forecast__list-item > strong'));
+                        return els.map(el => el.innerText);
                     });
-                    console.log(dataf);
+                                       
+                    // Add current weather to array
+                    f_icons.push(c_icon);
+                    f_icons_desc.push(c_desc);
+                    f_temp.push(c_temp);
+                    f_date.push(c_date);
+                                
+                    // Array to store weather (current and forecast)
+                    var weather = new Array();
+
+                    // Download icons
+                    for(let i = 0; i < f_icons.length; i++){  
+                        var viewSource = await page.goto(f_icons[i]);
+                        const icon_local = "./datas/icons/" + c_city + "_" + f_date[i].replace('.','_') + c_time + "_"+f_icons[i].split('/').pop();
+                        fs.writeFile(icon_local, await viewSource.buffer(), function(err) {
+                            if(err) {
+                                return console.log(err);
+                            }
+                        }); 
+                        
+                        // Convert values to object
+                        let obj = new Object();
+                        obj.city = c_city;
+                        obj.time = c_time;
+                        obj.temp = f_temp[i];
+                        obj.date = f_date[i].replace('.','');
+                        obj.desc = f_icons_desc[i];
+                        obj.icon = f_icons[i];
+                        obj.icon_local = icon_local;
+                        weather.push(obj);
+                    }
                     
-                    const c_image = await page.evaluate(el => el.src, await page.$('.weather-widget__actual-weather__img'));
-                    
-                    
-                    datas.push(c_image);
-                    console.log(datas);
-                    
-                    
-                    var viewSource = await page.goto(c_image);
-                    fs.writeFile("./" + c_image.split('/').pop(), await viewSource.buffer(), function(err) {
-                        if(err) {
-                        return console.log(err);
+                    // Save weather to json file
+                    fs.writeFile("./datas/"+ c_city + "_" + c_date.replace('.','_') + c_time + ".json", JSON.stringify(weather), 'utf8', function (err) {
+                        if (err) {
+                            return console.log(err);
                         }
-
-                        console.log("The file was saved!");
-                    });
-                    
-                    
-                    console.log(c_city + ' ' + c_desc + ' ' + c_temp +  ' ' + c_date + c_image);
-
+                    });                 
                 }
             }
+            console.log("Swiss meteo widget saved");
             await browser.close();
-            
 
+            } catch (error) {
+                console.log("Autocomplete list postal code error")
+            }
         }catch(error){
             console.log('Error: ', error)
 
@@ -124,5 +145,4 @@ const fs = require('fs');
         console.log('You must add a valid swiss postal code');
         process.exit(1);
     }
-    
 })();
